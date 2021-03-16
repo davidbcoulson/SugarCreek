321f81a3ac07e68019eb3c4e14aa69a1ad5bafca
Brad Renaud <bdrenaud86@gmail.com>
2021-03-15 19:53:38 -0500

    Second Commit, Adding Business Layer and Data Layer

---
+++ SugarCreek_DataLayer/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SugarCreek_DataLayer
+{
+    public class Class1
+    {
+    }
+}
