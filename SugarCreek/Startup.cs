6c94e620d8a6ecb3a2a865ad43ff29201bb0b2b1
Brad Renaud <bdrenaud86@gmail.com>
2021-03-15 19:42:06 -0500

    First Commit

---
+++ SugarCreek/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SugarCreek.Startup))]
+namespace SugarCreek
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
