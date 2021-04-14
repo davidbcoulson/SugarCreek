e8d86290065378598175e849ef0d950a409c2e2a
Brad Renaud <bdrenaud86@gmail.com>
2021-04-13 19:51:04 -0500

    Pulling

---
+++ SugarCreek/Controllers/GolfersController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SugarCreek.Controllers
+{
+    public class GolfersController : ApiController
+    {
+        [HttpGet]
+        public string ConnectionTest()
+        {
+            return "connection made";
+        }
+    }
+}
--- SugarCreek/Controllers/RoundsController.cs
+++ SugarCreek/Controllers/RoundsController.cs
@@ -1,14 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
+﻿using SugarCreek.Models;
+using SugarCreek_BusinessLayer.Models;
 using System.Web.Http;
 
 namespace SugarCreek.Controllers
 {
     public class RoundsController : ApiController
     {
+        [Route("api/rounds/createRound")]
+        [HttpPost]
+        [Authorize]
+        public GolfRound CreateGolfRound([FromBody] IncomingRoundRequest request )
+        {
+            return null
+
+        }
 
     }
 }
